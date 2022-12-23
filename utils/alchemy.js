99e53b173ab363c4880c58811d714b68846282a6
helloibm <butshikan@gmail.com>
2022-12-23 23:45:57 +0500

    env variable load issue - fix

--- utils/alchemy.js
+++ utils/alchemy.js
@@ -4,7 +4,7 @@ export function getAlchemy() {
     // Optional Config object, but defaults to demo api-key and eth-mainnet.
     const settings = {
         apiKey: process.env.ALCHEMY_KEY, // Replace with your Alchemy API Key.
-        network: process.env.NETWORK === 1 ? Network.ETH_MAINNET : Network.ETH_GOERLI, // Replace with your network.
+        network: process.env.NETWORK === '1' ? Network.ETH_MAINNET : Network.ETH_GOERLI, // Replace with your network.
     };
   
     return new Alchemy(settings);    
